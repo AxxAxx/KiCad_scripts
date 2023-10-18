06b13fb0b92fe0849b2feb6ce338d9652a9bce82
Axel Johansson <axel.johansson10@gmail.com>
2023-10-18 05:59:04 -0400

    Add files via upload

---
+++ kicad_create_filestructure.py
@@ -0,0 +1,21 @@
+import zipfile
+import os
+import shutil
+
+folder_structure = [['3rd-parties-libraries', '3D-models', 'componentsearchengine_ZIP_archive', 'footprints.pretty', 'schematic-symbols'], ['board-dimensions'], ['bom'], ['datasheets'], ['fabrication'], ['images'], ['logos'], ['pcb_render'], ['schematic_pdf']]
+
+currentpath = os.path.dirname(os.getcwd())
+print(currentpath)
+
+for folders in folder_structure:
+	tempfolderpath = os.path.join(currentpath, folders[0])	
+	if not os.path.exists(tempfolderpath):
+		os.makedirs(tempfolderpath)
+	if len(folders) > 1:
+		for sub_folders in folders:
+			tempsubfolderpath = os.path.join(tempfolderpath, sub_folders)
+			if not os.path.exists(tempsubfolderpath):
+				os.makedirs(tempsubfolderpath)
+				
+if not os.path.isfile(currentpath + "/3rd-parties-libraries/" + 'kicad_CSE_unpack.py'):
+                        os.replace(os.path.join(tempfolderpath + "/" + file, filename), currentpath + "/3D-models/" + filename)
