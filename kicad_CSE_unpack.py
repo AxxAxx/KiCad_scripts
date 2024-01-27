2b25d872f35ca9dcacc52b78ca086d154121ecf3
Axel Johansson <axeljohansson@duck.com>
2024-01-27 11:18:54 +0100

    Update script to unpack files into a separate folder for each component

--- kicad_CSE_unpack.py
+++ kicad_CSE_unpack.py
@@ -2,6 +2,7 @@ import zipfile
 import os
 import shutil
 
+# Function to remove a folder
 def remove(path):
     """ param <path> could either be relative or absolute. """
     if os.path.isfile(path) or os.path.islink(path):
@@ -12,59 +13,44 @@ def remove(path):
         raise ValueError("file {} is not a file or dir.".format(path))
 
 currentpath = os.getcwd()
-zipfolders = os.getcwd() + "/componentsearchengine_ZIP_archive"
+zipfolders = os.getcwd() + "/00_componentsearchengine_ZIP_archives"
 
-tempfolderpath = os.path.join(currentpath, "3D-models")
-if not os.path.exists(tempfolderpath):
-    os.makedirs(tempfolderpath)
-
-tempfolderpath = os.path.join(currentpath, "footprints.pretty")
-if not os.path.exists(tempfolderpath):
-    os.makedirs(tempfolderpath)
-
-tempfolderpath = os.path.join(currentpath,"schematic-symbols") 
-if not os.path.exists(tempfolderpath):
-    os.makedirs(tempfolderpath)
-
-# make temp folder
+# make temp folder for unzipping components
 tempfolderpath = os.path.join(currentpath, "temp")
 os.makedirs(tempfolderpath, exist_ok=True)
 print("temp directory created %s" % tempfolderpath)
 
+
 for filename in os.listdir(zipfolders):
     f = os.path.join(zipfolders, filename)
-    # checking if it is a file
+    # checking if it is a .zip file
     if f.endswith('.zip'):
-
+        # unzip
         with zipfile.ZipFile(f, 'r') as zip_ref:
             zip_ref.extractall(tempfolderpath)
-
-            if(1==1):#try:
+            
+            try:
+                componentfolderpath = os.path.join(currentpath,f.split('LIB_')[1].strip(".zip")) 
+                if not os.path.exists(componentfolderpath):
+                    os.makedirs(componentfolderpath)
+                # Move 3D model to component folder
                 file = f.split('LIB_')[1].strip(".zip")+"/3D"
-                print(file)
                 for filename in os.listdir(tempfolderpath + "/" + file):
-                    if not os.path.isfile(currentpath + "/3D-models/" + filename):
-                        os.replace(os.path.join(tempfolderpath + "/" + file, filename), currentpath + "/3D-models/" + filename)
+                    if not os.path.isfile(componentfolderpath + filename):
+                        os.replace(os.path.join(tempfolderpath + "/" + file, filename), os.path.join(componentfolderpath + "/" + filename))
                         print("moving file %s" % filename)
                     else:
                         print("%s did already exist!" % filename)
                 
+                # Move footprint and symbol to component folder                
                 file = f.split('LIB_')[1].strip(".zip")+"/KiCad"
                 for filename in os.listdir(tempfolderpath + "/" + file):
-                    if (filename.endswith('.lib') or filename.endswith('.kicad_sym')):
-                        if not os.path.isfile(currentpath + "/schematic-symbols/" + filename):
-                            os.replace(os.path.join(tempfolderpath + "/" + file, filename), currentpath + "/schematic-symbols/" + filename)
+                    if (filename.endswith('.kicad_mod') or filename.endswith('.kicad_sym')):
+                        if not os.path.isfile(componentfolderpath + filename):
+                            os.replace(os.path.join(tempfolderpath + "/" + file, filename), os.path.join(componentfolderpath + "/" + filename))
                             print("moving file %s" % filename)
                         else:
                             print("%s did already exist!" % filename)
                         
-                for filename in os.listdir(tempfolderpath + "/" + file):
-                    if filename.endswith('.kicad_mod') or filename.endswith('.mod'):
-                        if not os.path.isfile(currentpath + "/footprints.pretty/" + filename):
-                            os.replace(os.path.join(tempfolderpath + "/" + file, filename), currentpath + "/footprints.pretty/" + filename)
-                            print("moving file %s" % filename)
-                        else:
-                            print("%s did already exist!" % filename)
-            #except Exception as e: print(e)
+            except Exception as e: print(e)
 remove(tempfolderpath)
-print("temp directory removed %s" % tempfolderpath)
--- kicad_create_filestructure.py
+++ kicad_create_filestructure.py
@@ -2,7 +2,7 @@ import zipfile
 import os
 import shutil
 
-folder_structure = [['3rd-parties-libraries', '3D-models', 'componentsearchengine_ZIP_archive', 'footprints.pretty', 'schematic-symbols'], ['board-dimensions'], ['bom'], ['datasheets'], ['fabrication'], ['images'], ['logos'], ['pcb_render'], ['schematic_pdf']]
+folder_structure = [['3rd-parties-libraries', '00_componentsearchengine_ZIP_archives'], ['board-dimensions'], ['bom'], ['datasheets'], ['fabrication'], ['images'], ['logos'], ['pcb_render'], ['schematic_pdf']]
 
 currentpath = os.path.dirname(os.getcwd())
 
@@ -11,10 +11,10 @@ for folders in folder_structure:
 	if not os.path.exists(tempfolderpath):
 		os.makedirs(tempfolderpath)
 	if len(folders) > 1:
-		for sub_folders in folders:
+		for sub_folders in folders[1:]:
 			tempsubfolderpath = os.path.join(tempfolderpath, sub_folders)
 			if not os.path.exists(tempsubfolderpath):
 				os.makedirs(tempsubfolderpath)
 				
 if not os.path.isfile(currentpath + '/3rd-parties-libraries' + '/kicad_CSE_unpack.py'):
-                        shutil.copyfile(os.path.join(os.getcwd()) + '/kicad_CSE_unpack.py', currentpath + '/3rd-parties-libraries' + '/kicad_CSE_unpack.py')
+                        shutil.copyfile(os.path.join(os.getcwd()) + '/kicad_CSE_unpack.py', currentpath + '/3rd-parties-libraries' + '/00_kicad_CSE_unpack.py')
